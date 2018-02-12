17e6f07edf66ba39e147a8806975649fdffe7480
violet-g <gabriella.georgieva17@gmail.com>
2018-02-12 20:57:58 +0000

    Style variable table

--- client/src/components/stage/VariableTable.jsx
+++ client/src/components/stage/VariableTable.jsx
@@ -2,16 +2,25 @@ import React from 'react'
 
 const VariableTable = ({ variables, onVariableAdd, onStepAdd }) => (
   <div className="VariableTable">
-    {variables.map((variable, i) =>
-      <div key={i} className="col">
-        <div className="cell">TODO variable name input</div>
-        {variable.steps.map((step, j) =>
-          <div className="cell" key={[i, j].join('_')}>TODO step inputs</div>
-        )}
-        <div onClick={() => onStepAdd(i)}>[+]</div>
-      </div>
-    )}
-    <div className="col" onClick={onVariableAdd}>[+]</div>
+    <button className="btn btn-default btn-add-col" onClick={onVariableAdd}>[+]</button>
+      <div className="variable-table">
+        {variables.map((variable, i) =>
+          <div key={i} className="variable-table-col">
+            <div className="cell"><input type="text" className="form-control" placeholder="Name..." /></div>
+              {variable.steps.map((step, j) =>
+                <div className="cell" key={[i, j].join('_')}>
+                  <div className="form-group">
+                    <input type="text" className="form-control" placeholder="Line..." />
+                  </div>
+                  <div className="form-group">
+                    <input type="text" className="form-control" placeholder="Value..." />
+                  </div>
+                </div>
+              )}
+              <button class="btn btn-default" onClick={() => onStepAdd(i)}>[+]</button>
+            </div>
+          )}
+        </div>
   </div>
 )
 
