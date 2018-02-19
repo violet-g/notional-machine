498381dc859918746b40cde91f372221fe53c760
violet-g <gabriella.georgieva17@gmail.com>
2018-02-19 17:16:38 +0000

    Delete unused components

--- client/src/components/old-implementations/Task.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Task = ({ task }) => (
-  <p>{task}</p>
-)
-
-export default Task
--- client/src/components/old-implementations/first-scribbles/DeleteExprButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DeleteExprButton = ({ onDelete }) => (
-  <span className="DeleteExprButton" onClick={onDelete}>
-    <span aria-hidden="true">&times;</span>
-  </span>
-)
-
-export default DeleteExprButton
