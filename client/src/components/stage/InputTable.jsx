a4d87015f917f013fdbd7d3b60589f9c277295bb
violet-g <gabriella.georgieva17@gmail.com>
2018-02-28 10:01:09 +0000

    Update input line visualisation

--- client/src/components/stage/InputTable.jsx
+++ client/src/components/stage/InputTable.jsx
@@ -7,7 +7,7 @@ const InputTable = ({ inputs }) => (
       {inputs.map((input, i) =>
         <li key={i} className="list-group-item">
           <div className="row">
-            <div className="col-sm-5">{input[0]}</div>
+            <div className="col-sm-5">Step {input[0]}:</div>
             <div className="col-sm-2 text-center">/</div>
             <div className="col-sm-5 text-right">{input[1]}</div>
           </div>
